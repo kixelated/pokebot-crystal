81024b3559f504f46d164f4429c5101aa6ad60ce
kixelated <kixelated@gmail.com>
2017-10-01 00:31:36 -0700

    working emulator?

--- gambatte_wrapper.nim
+++ gambatte_wrapper.nim
@@ -1,42 +1,12 @@
-{.passC: "-Igambatte/libgambatte/src".}
-{.passC: "-Igambatte/libgambatte/include".}
-{.passC: "-Igambatte/common".}
+{.passC: "-Igambatte/libgambatte/include -DHAVE_STDINT_H".}
+{.passL: "-Lgambatte/libgambatte -lgambatte -lz".}
 
-{.compile: "gambatte/libgambatte/src/bitmap_font.cpp".}
-{.compile: "gambatte/libgambatte/src/cpu.cpp".}
-{.compile: "gambatte/libgambatte/src/gambatte.cpp".}
-{.compile: "gambatte/libgambatte/src/initstate.cpp".}
-{.compile: "gambatte/libgambatte/src/interrupter.cpp".}
-{.compile: "gambatte/libgambatte/src/interruptrequester.cpp".}
-{.compile: "gambatte/libgambatte/src/loadres.cpp".}
-{.compile: "gambatte/libgambatte/src/memory.cpp".}
-{.compile: "gambatte/libgambatte/src/sound.cpp".}
-{.compile: "gambatte/libgambatte/src/state_osd_elements.cpp".}
-{.compile: "gambatte/libgambatte/src/statesaver.cpp".}
-{.compile: "gambatte/libgambatte/src/tima.cpp".}
-{.compile: "gambatte/libgambatte/src/video.cpp".}
-{.compile: "gambatte/libgambatte/src/mem/cartridge.cpp".}
-{.compile: "gambatte/libgambatte/src/mem/memptrs.cpp".}
-{.compile: "gambatte/libgambatte/src/mem/pakinfo.cpp".}
-{.compile: "gambatte/libgambatte/src/mem/rtc.cpp".}
-{.compile: "gambatte/libgambatte/src/sound/channel1.cpp".}
-{.compile: "gambatte/libgambatte/src/sound/channel2.cpp".}
-{.compile: "gambatte/libgambatte/src/sound/channel3.cpp".}
-{.compile: "gambatte/libgambatte/src/sound/channel4.cpp".}
-{.compile: "gambatte/libgambatte/src/sound/duty_unit.cpp".}
-{.compile: "gambatte/libgambatte/src/sound/envelope_unit.cpp".}
-{.compile: "gambatte/libgambatte/src/sound/length_counter.cpp".}
-{.compile: "gambatte/libgambatte/src/video/ly_counter.cpp".}
-{.compile: "gambatte/libgambatte/src/video/lyc_irq.cpp".}
-{.compile: "gambatte/libgambatte/src/video/next_m0_time.cpp".}
-{.compile: "gambatte/libgambatte/src/video/ppu.cpp".}
-{.compile: "gambatte/libgambatte/src/video/sprite_mapper.cpp".}
-{.compile: "gambatte/libgambatte/src/file/file.cpp".}
-
-type size_t* {.importcpp: "size_t".} = uint
-type uint_least32_t {.importcpp: "uint_least32_t".} = uint32 # TODO
+type size_t* {.importcpp: "std::size_t".} = uint
+type uint_least32_t* {.importcpp: "gambatte::uint_least32_t".} = uint32 # TODO
+type std_uint_least32_t* {.importcpp: "std::uint_least32_t".} = uint32 # TODO
+type ptrdiff_t* {.importcpp: "std::ptrdiff_t".} = int32
 
 type GB* {.header: "gambatte.h", importcpp: "gambatte::GB".} = object
 proc reset*(this: var GB) {.header: "gambatte.h", importcpp: "#.reset()".}
 proc load*(this: var GB, romfile: cstring, flags: cint = 0): cint {.header: "gambatte.h", importcpp: "#.load(@)".}
-proc runFor*(this: var GB, videoBuf: pointer, pitch: cint, audioBuf: pointer, samples: var size_t): cint {.header: "gambatte.h", importcpp: "#.runFor(@)".}
+proc runFor*(this: var GB, videoBuf: pointer, pitch: ptrdiff_t, audioBuf: pointer, samples: var size_t): ptrdiff_t {.header: "gambatte.h", importcpp: "#.runFor(@)".}
--- main.nim
+++ main.nim
@@ -6,22 +6,19 @@ discard gb.load("rom/crystal.gbc")
 
 discard sdl2.init(sdl2.INIT_AUDIO or sdl2.INIT_VIDEO)
 
-var window = createWindow("SDL Skeleton", sdl2.SDL_WINDOWPOS_UNDEFINED, sdl2.SDL_WINDOWPOS_UNDEFINED, 320, 288, SDL_WINDOW_SHOWN)
+var window = createWindow("Nimtendo", sdl2.SDL_WINDOWPOS_UNDEFINED, sdl2.SDL_WINDOWPOS_UNDEFINED, 320, 288, SDL_WINDOW_SHOWN)
 var renderer = createRenderer(window, -1, 0)
 var texture = createTexture(renderer, sdl2.SDL_PIXELFORMAT_ARGB8888, sdl2.SDL_TEXTUREACCESS_STREAMING, 160, 144);
-var surface = createRGBSurface(160, 144, 8)
+
+renderer.setDrawColor(0, 0, 0, 255)
+renderer.clear
 
 var evt = sdl2.defaultEvent
 var runGame = true
 
 var bufSamples = 0
 
-var pixels: array[0..(160*144)*2, uint32]
-var audio: array[0..(35112+2064)*2, uint32]
-
-discard renderer.setLogicalSize(160, 144)
-renderer.setDrawColor(0, 0, 0, 255)
-renderer.clear
+var audio = alloc(35112+2064 * sizeof(uint_least32_t))
 
 while runGame:
   while pollEvent(evt):
@@ -29,19 +26,21 @@ while runGame:
       runGame = false
       break
 
+  var pixels: pointer
+  var pitch: cint
+  discard texture.lockTexture(nil, addr pixels, addr pitch)
+
   var doneSamples = -1
   while doneSamples == -1:
     var runSamples: size_t = 35112
-    doneSamples = gb.runFor(addr pixels, 160, addr audio, runSamples)
+    doneSamples = gb.runFor(pixels, 160, audio, runSamples)
 
-  for i in countup(1, 160*144):
-    pixels[i] = pixels[i*2]
+  texture.unlockTexture
 
-  texture.updateTexture(nil, addr pixels, 160*4)
-
-  renderer.clear
   renderer.copy(texture, nil, nil)
   renderer.present
+
+dealloc audio
 
 destroy window
 destroy renderer
