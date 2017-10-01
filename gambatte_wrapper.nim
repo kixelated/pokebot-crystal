baf1c58ff4c4aeade54fc11e8fcbd5062ee5ad86
kixelated <kixelated@gmail.com>
2017-10-01 02:34:46 -0700

    Added button input.

--- gambatte_wrapper.nim
+++ gambatte_wrapper.nim
@@ -7,6 +7,20 @@ type std_uint_least32_t* {.importcpp: "std::uint_least32_t".} = uint32 # TODO
 type ptrdiff_t* {.importcpp: "std::ptrdiff_t".} = int32
 
 type GB* {.header: "gambatte.h", importcpp: "gambatte::GB".} = object
+
 proc reset*(this: var GB) {.header: "gambatte.h", importcpp: "#.reset()".}
 proc load*(this: var GB, romfile: cstring, flags: cint = 0): cint {.header: "gambatte.h", importcpp: "#.load(@)".}
 proc runFor*(this: var GB, videoBuf: pointer, pitch: ptrdiff_t, audioBuf: pointer, samples: var size_t): ptrdiff_t {.header: "gambatte.h", importcpp: "#.runFor(@)".}
+
+proc getInput*(this: var GB): cuint {.header: "gambatte.h", importcpp: "#.getInput()".}
+proc setInput*(this: var GB, button: cuint) {.header: "gambatte.h", importcpp: "#.setInput(@)".}
+
+const
+  BUTTON_A*: cuint = 0x01
+  BUTTON_B*: cuint = 0x02
+  BUTTON_SELECT*: cuint = 0x04
+  BUTTON_START*: cuint = 0x08
+  BUTTON_RIGHT*: cuint = 0x10
+  BUTTON_LEFT*: cuint = 0x20
+  BUTTON_UP*: cuint = 0x40
+  BUTTON_DOWN*: cuint = 0x80
--- main.nim
+++ main.nim
@@ -1,30 +1,63 @@
-import gambatte_wrapper
+import gambatte_wrapper as gambatte
 import sdl2
 
 var gb: GB
 discard gb.load("rom/crystal.gbc")
 
-discard sdl2.init(sdl2.INIT_AUDIO or sdl2.INIT_VIDEO)
+discard gb.getInput()
 
-var window = createWindow("Nimtendo", sdl2.SDL_WINDOWPOS_UNDEFINED, sdl2.SDL_WINDOWPOS_UNDEFINED, 320, 288, SDL_WINDOW_SHOWN)
-var renderer = createRenderer(window, -1, 0)
-var texture = createTexture(renderer, sdl2.SDL_PIXELFORMAT_ARGB8888, sdl2.SDL_TEXTUREACCESS_STREAMING, 160, 144);
+discard sdl2.init(INIT_VIDEO)
+
+var window = sdl2.createWindow("Nimtendo", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 320, 288, SDL_WINDOW_SHOWN)
+var renderer = window.createRenderer(-1, 0)
+var texture = renderer.createTexture(SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_STREAMING, 160, 144);
 
 renderer.setDrawColor(0, 0, 0, 255)
-renderer.clear
+renderer.clear()
 
-var evt = sdl2.defaultEvent
+var evt = defaultEvent
 var runGame = true
 
 var bufSamples = 0
 
-var audio = alloc(35112+2064 * sizeof(uint_least32_t))
+var audio = alloc((35112 + 2064) * sizeof(uint_least32_t))
+
+proc getButton(key: cint): cuint =
+  case key:
+    of K_d:
+      return gambatte.BUTTON_A
+    of K_c:
+      return gambatte.BUTTON_B
+    of K_RSHIFT:
+      return gambatte.BUTTON_SELECT
+    of K_RETURN:
+      return gambatte.BUTTON_START
+    of K_RIGHT:
+      return gambatte.BUTTON_RIGHT
+    of K_LEFT:
+      return gambatte.BUTTON_LEFT
+    of K_UP:
+      return gambatte.BUTTON_UP
+    of K_DOWN:
+      return gambatte.BUTTON_DOWN
+    else:
+      return 0
 
 while runGame:
   while pollEvent(evt):
-    if evt.kind == QuitEvent:
-      runGame = false
-      break
+    case evt.kind:
+      of QuitEvent:
+        runGame = false
+        break
+      of KeyDown:
+        let button = getButton(evt.key.keysym.sym)
+        if button != 0:
+          gb.setInput(gb.getInput() or button)
+      of KeyUp:
+        let button = getButton(evt.key.keysym.sym)
+        if button != 0:
+          gb.setInput(gb.getInput() and not button)
+      else: discard
 
   var pixels: pointer
   var pitch: cint
@@ -35,13 +68,15 @@ while runGame:
     var runSamples: size_t = 35112
     doneSamples = gb.runFor(pixels, 160, audio, runSamples)
 
-  texture.unlockTexture
+  texture.unlockTexture()
 
   renderer.copy(texture, nil, nil)
-  renderer.present
+  renderer.present()
 
-dealloc audio
+dealloc(audio)
 
-destroy window
-destroy renderer
-destroy texture
+destroy(window)
+destroy(renderer)
+destroy(texture)
+
+sdl2.quit()
